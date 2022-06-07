9a7b81ef5b26dc202d41f651595bfc77ea71816b
Divyanshu Lohar <Divyanshulohar2002@gmail.com>
2022-06-07 13:31:32 +0530

    hello

---
+++ matrixSearch.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int count = 0;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+		
+	}
+	for(int i=0;i<n;i++){
+		int subXor= 0;
+		int surArr= 0;
+		for(int j=i;j<n;j++){
+			subXor^= arr[j];
+			surArr+= arr[j];
+		}
+		if(subXor == surArr){
+			count++;
+		}
+
+
+	}
+	cout<<cout<<endl;
+	
+
+
+	return 0;
+}
