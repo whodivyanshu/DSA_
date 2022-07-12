fd6857eb3141b96f0ce4a53e53ff475488761b31
Divyanshu Lohar <divyanshulohar2002@gmail.com>
2022-07-12 12:49:58 +0530

    pattern9

--- patter.cpp
+++ patter.cpp
@@ -3,13 +3,18 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=0;i<n;i++){
+    // string str1 = " ";
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n-1;j>i;j--)
+        {
+            cout<<" ";
+        }
         for(int j=0;j<=i;j++){
             cout<<"*";
         }
         cout<<endl;
+        
     }
-
-
     return 0;
 }
