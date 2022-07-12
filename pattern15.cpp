2f7767456a87356c95090cce0ffa3ce39237d984
Divyanshu Lohar <divyanshulohar2002@gmail.com>
2022-07-12 22:25:14 +0530

    patter15

--- pattern15.cpp
+++ pattern15.cpp
@@ -7,15 +7,34 @@ int main(){
         for(int j=0;j<=i;j++){
             cout<<"*"<<" ";
         }
-        for(int j=n-1;j>=i;j--){
+        for(int j=n-1;j>i;j--){
             cout<<" "<<" ";
         }
-        for(int j=n-2;j>=i;j--){
+        for(int j=n-1;j>=i+1;j--){
             cout<<" "<<" ";
         }
         for(int j=0;j<=i;j++){
             cout<<"*"<<" ";
         }
+        
+        
+        cout<<endl;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>=i+1;j--){
+            cout<<"*"<<" ";
+        }
+        for(int j=0;j<=i;j++){
+            cout<<" "<<" ";
+        }
+        for(int j=0;j<=i;j++){
+            cout<<" "<<" ";
+        }
+        for(int j=n-1;j>i;j--){
+            cout<<"*"<<" ";
+        }
+        
+        
         cout<<endl;
     }
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,26 @@
-(i+j)%2
+for(int i=0;i<n;i++){
+    //     for(int j=n;j>i;j--){
+    //         cout<<"*"<<" ";
+    //     }
+    //     for(int j=0;j<=i;j++){
+    //         cout<<" "<<" ";
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         cout<<" "<<" ";
+    //     }
+    //     for(int j=n-1;j>i;j--){
+    //         cout<<"*"<<" ";
+    //     }
+    //     // for(int j=n-1;j>i;j--){
+    //     //     cout<<" "<<" ";
+    //     // }
+    //     // for(int j=n-1;j>i;j--){
+    //     //     cout<<" "<<" ";
+    //     // }
+    //     // for(int j=0;j<=i;j++){
+    //     //     cout<<"*"<<" ";
+    //     // }
+        
+        
+    //     cout<<endl;
+    // }
