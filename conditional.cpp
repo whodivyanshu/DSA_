970d5ffd1136184d11970faeac82c0c31f0b0651
Divyanshu Lohar <divyanshulohar2002@gmail.com>
2022-07-13 11:41:55 +0530

    committed

--- conditional.cpp
+++ conditional.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a=10, b=20;
-    (a>b)?cout<<"YES":cout<<"No";
-
-
+    int a=20, b=20;
+    (a>b)?cout<<"YES":(a<b)?cout<<"No":cout<<"YES";
     return 0;
 }
