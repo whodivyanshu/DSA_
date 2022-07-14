4d48d23776eff7771162947403295fd070279d5c
Divyanshu Lohar <divyanshulohar2002@gmail.com>
2022-07-14 11:46:36 +0530

    factorial

--- factorial.cpp
+++ factorial.cpp
@@ -1,22 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int sum = 1;
     int ll;
     int ul;
-    cin>>ll;
-    cin>>ul;
-    for(int i=1;i<=n;i++){
-        
-        sum*=i;
-        if(sum>=ll && sum<=ul){
-            cout<<i<<endl;
+    cin >> ll;
+    cin >> ul;
+    int sum2 = 0;
+    int count = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (sum > ul)
+        {
+            break;
+        }
+        sum *= i;
+
+        if (sum >= ll && sum <= ul)
+        {
+            cout<<sum<<endl;
+            sum2 += sum;
+            count = i;
+            
         }
     }
-    // cout<<sum<<endl;
 
+    cout<<count<<endl;
+    cout<<sum2<<endl;
 
     return 0;
 }
